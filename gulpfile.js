1f83a79573d8bebf4608315ba28142db299f3a0d
zj <940233351@qq.com>
2018-06-11 17:23:23 +0800

    项目运行环境构建完成

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,12 @@ var css = require('gulp-clean-css'); //压缩css
 var webserver = require('gulp-webserver'); //起服务
 var js = require('gulp-uglify'); //压缩js
 var clean = require('gulp-clean') //删除文件
+var html = require('gulp-htmlmin')
 var concat = require('gulp-concat') // 合并文件
 var path = require('path');
 var url = require('url');
 var fs = require('fs');
+//起服务
 gulp.task('srever', function() {
     gulp.src('src')
         .pipe(webserver({
@@ -17,12 +19,32 @@ gulp.task('srever', function() {
             livereload: true,
             middleware: function(req, res, next) {
                 var pathname = url.parse(req.url).pathname;
+                console.log(pathname);
                 if (pathname === '/favicon.ico') {
                     return false;
-                } else {
-                    pathname = pathname === "/" ? 'index.html' : pathname;
                 }
-
+                pathname = pathname === "/" ? 'index.html' : pathname;
+                res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)));
             }
-        }))
-})
+        }));
+});
+//压缩合并css
+gulp.task('sass', function() {
+    gulp.src('src/**/*.scss')
+        .pipe(sass())
+        .pipe(concat('style.css'))
+        .pipe(css())
+        .pipe(gulp.dest('style'))
+});
+//压缩合并js
+gulp.task('js', function() {
+    gulp.src('src/**/*.js')
+        .pipe(concat('style.js'))
+        .pipe(js())
+        .pipe(gulp.dest('style'));
+});
+//操作html文件
+gulp.task('html', function() {
+    gulp.src('src/index.html')
+        .pipe(gulp.dest('style'));
+});
